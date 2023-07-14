e973baab44235011266f266cd8443ea7e31c11d0
agsafronenko <109076625+agsafronenko@users.noreply.github.com>
2023-07-14 11:24:43 +0200

    mongodb connection

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,22 @@
-'use strict';
+"use strict";
 
 module.exports = function (app) {
+  app
+    .route("/api/issues/:project")
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+    .get(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
     });
-    
 };
